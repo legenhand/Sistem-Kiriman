b248c967b528a9f62b6d3919e7c0822a899027dc
unknown <firmansyah720.fs@gmail.com>
2019-07-19 21:33:01 +0700

    ngatur header

--- index.php
+++ index.php
@@ -67,12 +67,20 @@
             }
             $.validate({
             });
+            function tampilkanwaktu(){         //fungsi ini akan dipanggil di bodyOnLoad dieksekusi tiap 1000ms = 1detik    
+                var waktu = new Date();            //membuat object date berdasarkan waktu saat 
+                var sh = waktu.getHours() + "";    //memunculkan nilai jam, //tambahan script + "" supaya variable sh bertipe string sehingga bisa dihitung panjangnya : sh.length    //ambil nilai menit
+                var sm = waktu.getMinutes() + "";  //memunculkan nilai detik    
+                var ss = waktu.getSeconds() + "";  //memunculkan jam:menit:detik dengan menambahkan angka 0 jika angkanya cuma satu digit (0-9)
+                document.getElementById("clock").innerHTML = (sh.length==1?"0"+sh:sh) + ":" + (sm.length==1?"0"+sm:sm) + ":" + (ss.length==1?"0"+ss:ss);
+            }
         </script>
         
     </head>
-    <body>
+    <body onload="tampilkanwaktu();setInterval('tampilkanwaktu()', 1000);">
         <header>
-            Aplikasi Manajemen kiriman
+            Aplikasi Manajemen kiriman 
+            <?= date('l, d-m-Y  '); ?><span id="clock"></span>
         </header>
         <div class="container">
             <aside>
