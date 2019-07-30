0ee864107459094585a1d3fe19a2dcca062aeeba
Legenhand <firmansyah720.fs@gmail.com>
2019-07-30 09:18:11 +0700

    login bootstrap

--- login.php
+++ login.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" class="h-100">
     <head>
         <title>Login Aplikasi</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="stylesheet" href="css/style.css">   
+        <meta name="viewport" content="width=device-width,initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <script src="js/jquery-3.4.1.min.js"></script> 
     </head>
-    <body>
+    <body class="h-100 bg-info d-flex align-items-center">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
         <div class="container">
-            <section class="login-box">
-                <h2>Login Aplikasi</h2>
-                <form action="ceklogin.php" method="post">
-                    <input type="text" placeholder="Username" id="username" name="username" data-validation="required">
-                    <input type="password" placeholder="Password" id="password" name="password" data-validation="required">
-                    <input type="submit" value="LOGIN" id="login">
-                </form>
+            <div class="row">
+                <div class="col-sm-6 col-md-4 mx-auto bg-light p-4">
+                    <h3 class="text-center text-info pb-3 mb-3 border-bottom">Login Aplikasi</h3>
+                    <form action="ceklogin.php" method="post">
+                        <input class="form-control form-control-lg mb-3" type="text" placeholder="Username" id="username" name="username" data-validation="required">
+                        <input class="form-control form-control-lg mb-3" type="password" placeholder="Password" id="password" name="password" data-validation="required">
+                        <input class="btn btn-info btn-lg btn-block" type="submit" value="LOGIN" id="login">
+                    </form>
+                </div>
+            </div>
         </div>
         
             <script src="js/form-validator/jquery.form-validator.min.js"></script>
